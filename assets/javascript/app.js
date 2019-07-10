9c3706d8add57ab637ffbd3bd376237054b3e5c3
Dealan Diongzon <dealan.diongzon@gmail.com>
2019-07-10 19:13:49 -0400

    added suggested topics into an array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,3 @@
-$("#test").html("hi!")
+//variables
+var topics = ["rabbits", "bunnies", "buster baxter", "judy hopps", "bugs bunny", 
+"energizer bunny", "easter bunny", "cartoon bunny", "bunny ears", "white rabbit"]
