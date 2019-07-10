f0f61071b30869a9b81a8f47fa2b608ad8e78b1b
Dealan Diongzon <dealan.diongzon@gmail.com>
2019-07-10 19:28:53 -0400

    added all the topics in buttons when the page first renders

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,21 @@
-//variables
+//VARIABLES
 var topics = ["rabbits", "bunnies", "buster baxter", "judy hopps", "bugs bunny", 
 "energizer bunny", "easter bunny", "cartoon bunny", "bunny ears", "white rabbit"]
+
+//FUNCTIONS
+
+function renderButtons() {
+    //creates a var for the empty div for the buttons
+    var buttonDiv = $("#gif-buttons-div");
+
+    //Deletes content inside
+    buttonDiv.empty();
+
+    //loop through an array of topics and generate button for each topic
+    for (var i=0; i<topics.length;i++){
+        buttonDiv.append('<button type="button" class="btn btn-primary btn-sm" id="gif-button">' + topics[i] + '</button>'); 
+    }
+}
+
+//MAIN APP
+renderButtons();
