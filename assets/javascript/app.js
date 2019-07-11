71d68521a30cbf37eac8505ec855aceaed1bb450
Dealan Diongzon <dealan.diongzon@gmail.com>
2019-07-11 03:09:01 -0400

    added the function that displays the rating and the gifs after clicking any button already listed on the site

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,11 @@
 //VARIABLES
-var topics = ["rabbits", "bunnies", "buster baxter", "judy hopps", "bugs bunny", 
-"energizer bunny", "easter bunny", "cartoon bunny", "bunny ears", "white rabbit"]
+var topics = ["rabbits", "bunnies", "buster baxter", "judy hopps", "bugs bunny",
+    "energizer bunny", "easter bunny", "cartoon bunny", "bunny ears", "white rabbit"
+]
 
 //HTML VARIABLES
 var buttonDiv = $("#gif-buttons-div");
+var gifDiv = $("#gif-imgs-div");
 
 //FUNCTIONS
 
@@ -13,13 +15,13 @@ function renderButtons() {
     buttonDiv.empty();
 
     //loop through an array of topics and generate button for each topic
-    for (var i=0; i<topics.length;i++){
+    for (var i = 0; i < topics.length; i++) {
         //creates the button
         var button = $("<button>");
 
         //adds bootstrap class
         button.addClass("btn btn-primary btn sm");
-        
+
         //adds css style id
         button.attr("id", "gif-button");
 
@@ -30,7 +32,7 @@ function renderButtons() {
         button.text(topics[i]);
 
         //adds button to HTML
-        buttonDiv.append(button); 
+        buttonDiv.append(button);
     }
 }
 
@@ -39,15 +41,15 @@ renderButtons();
 
 //BUTTON CLICK FUNCTIONS
 //WHEN USER CLICKS ON RECOMMENDED BUTTONS
-$(document).on('click', "#gif-button", function(){
+$(document).on('click', "#gif-button", function () {
 
     //create a new variable that stores the data-attribute of the topic
     var topicName = $(this).data("name");
-    
+
     //api key variable and queryURL variables
     var api = "SoRSlrz9spbXDknc99YFPDDvVZrNd9w9";
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + api + "&q=" + topicName + "&limit=10";
-    
+
     //AJAX CALL
     $.ajax({
         url: queryURL,
@@ -55,17 +57,31 @@ $(document).on('click', "#gif-button", function(){
     }).then(function (response) {
         console.log(queryURL);
         console.log(response);
-    });
-    //DISLAY THE GIFS
 
-    
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+            
+            var div = $('<div>');
+            var p = $('<p>');
+            p.text("Rating: " + results[i].rating);
+
+            var bunnyImage = $("<img>");
+            bunnyImage.attr("src", results[i].images.fixed_height.url);
+
+            div.append(p);
+            div.append(bunnyImage);
+            gifDiv.prepend(div);
+
+        }
+    });
+
+
 })
 
 //WHEN USER CLICKS ON SEARCH FUNCTIONS
 //function to add the button to the button div
-    //pulls the data-attribute and stores it in a variable
-    //NEED THE GIFPY LINK
-    //AJAX CALL
-    //DISLAY THE GIFS
-
-
+//pulls the data-attribute and stores it in a variable
+//NEED THE GIFPY LINK
+//AJAX CALL
+//DISLAY THE GIFS
