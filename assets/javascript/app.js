707ff7768612315cb5885dd71e225ab6186f56ef
Dealan Diongzon <dealan.diongzon@gmail.com>
2019-07-10 20:01:48 -0400

    changed the logic of pushing the buttons to the HTML so that the code would also assign each button a unique data-attribute

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,20 +2,45 @@
 var topics = ["rabbits", "bunnies", "buster baxter", "judy hopps", "bugs bunny", 
 "energizer bunny", "easter bunny", "cartoon bunny", "bunny ears", "white rabbit"]
 
+//HTML VARIABLES
+var buttonDiv = $("#gif-buttons-div");
+
 //FUNCTIONS
 
 function renderButtons() {
-    //creates a var for the empty div for the buttons
-    var buttonDiv = $("#gif-buttons-div");
 
-    //Deletes content inside
+    //Deletes content inside button Div
     buttonDiv.empty();
 
     //loop through an array of topics and generate button for each topic
     for (var i=0; i<topics.length;i++){
-        buttonDiv.append('<button type="button" class="btn btn-primary btn-sm" id="gif-button">' + topics[i] + '</button>'); 
+        //creates the button
+        var button = $("<button>");
+
+        //adds bootstrap class
+        button.addClass("btn btn-primary btn sm");
+        
+        //adds css style id
+        button.attr("id", "gif-button");
+
+        //adds data-attribute
+        button.attr("data-name", topics[i]);
+
+        //adds HTML text to button
+        button.text(topics[i]);
+
+        //adds button to HTML
+        buttonDiv.append(button); 
     }
 }
 
 //MAIN APP
 renderButtons();
+
+//BUTTON CLICK FUNCTIONS
+button.on('click', function(){
+    //Deletes content inside button Div
+    buttonDiv.empty();
+
+    
+})
